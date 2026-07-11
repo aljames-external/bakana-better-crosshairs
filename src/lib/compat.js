d9e86ffc546713974c6cdfb859bde6cb4b2b6708
Bakana Baka <bakanabaka.vtt@gmail.com>
2026-07-11 02:15:11 +0000

    cleanup: resolve deprecation warning

--- src/lib/compat.js
+++ src/lib/compat.js
@@ -3,5 +3,6 @@
  */
 
 export const Token = foundry.canvas.placeables.Token;
+export const Ray = foundry.canvas.geometry.Ray;
 
-export const Ray = foundry.canvas?.geometry?.Ray ?? globalThis.Ray;
+export const clearHighlightLayer = canvas.interface.grid.clearHighlightLayer;
