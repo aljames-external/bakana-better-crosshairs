4ff471e93e7e6ea9391b622f30f35ccd27736ac3
Alexander James <aljames@google.com>
2026-07-12 21:31:42 +0000

    fix: import getGridSnapMode in cone, ray, square, and circle modules

--- src/crosshair/square.js
+++ src/crosshair/square.js
@@ -1,7 +1,7 @@
 import { closest } from "../lib/filemanager.js";
 import { log } from "../lib/logger.js";
 import { crosshairAdapter } from "../adapter/foundry/index.js";
-import { resolveCrosshairPlacement, attachWheelRotation, detachWheelRotation, shouldStickToToken, resolveCrosshairIcon, alignCrosshairAndEffects } from "./util.js";
+import { resolveCrosshairPlacement, attachWheelRotation, detachWheelRotation, shouldStickToToken, resolveCrosshairIcon, alignCrosshairAndEffects, getGridSnapMode } from "./util.js";
 
 /**
  * Creates and configures a square crosshair sequence.
