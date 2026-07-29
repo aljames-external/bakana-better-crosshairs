f1342bca696998f05fbdc7c86e1ec352a3b21c93
Alexander James <aljames@google.com>
2026-07-29 03:31:58 +0000

    fix: Restore remoteCrosshairManager.js from almost-working branch

--- src/crosshair/remoteCrosshairManager.js
+++ src/crosshair/remoteCrosshairManager.js
@@ -266,7 +266,7 @@ export class RemoteCrosshairVisual {
             .name(this.effectName)
             .file(effectFile)
             .atLocation({ x: this.rawX, y: this.rawY })
-            .rotate(deg)
+            .rotate(rad)
             .anchor(this.shape?.animationAnchor ?? { x: 0.5, y: 0.5 })
             .size({ width: widthPx * factor, height: heightPx * factor }, { gridUnits: Boolean(gridUnits) })
             .opacity(0.8)
@@ -299,13 +299,12 @@ export class RemoteCrosshairVisual {
 
         log.debug(`[Bakana Remote Socket Update] Sender: "${this.senderUserId}" | Origin: (${this.rawX}, ${this.rawY}) | Cursor: (${this.cursorX}, ${this.cursorY}) | Direction: ${this.rawDirection}°`);
 
-        const deg = this.rawDirection ?? 0;
-        const rad = deg * (Math.PI / 180);
+        const rad = (this.rawDirection ?? 0) * (Math.PI / 180);
 
         if (this.shape) {
             this.shape.x = this.rawX;
             this.shape.y = this.rawY;
-            this.shape.direction = deg;
+            this.shape.direction = this.rawDirection;
         }
 
         if (Sequencer.EffectManager) {
@@ -321,7 +320,7 @@ export class RemoteCrosshairVisual {
                     eff.position.x = this.rawX;
                     eff.position.y = this.rawY;
                 }
-                eff.rotation = deg;
+                eff.rotation = rad;
 
                 if (eff.container) {
                     if (eff.container.position?.set) {
@@ -337,7 +336,7 @@ export class RemoteCrosshairVisual {
                     try {
                         eff.update({
                             position: { x: this.rawX, y: this.rawY },
-                            rotation: deg
+                            rotation: rad
                         });
                     } catch (e) {}
                 }
